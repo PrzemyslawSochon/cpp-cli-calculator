71196d6dad3a19a1e75bba0a50aee17fb99d9814
PrzemyslawSochon <przemyslaw.sochon+github@protonmail.ch>
2022-12-11 07:37:50 +0100

    can't remember

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include "input.hpp"
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+std::string filterWhitespace(const std::string& str)
+{
+    auto is_ws = [](char c)
+    {
+        return std::isspace(c);
+    };
+    std::string copy{str};
+    copy.erase(std::remove_if(copy.begin(), copy.end(), is_ws), copy.end());
+
+    return copy;
+}
 
 int main()
 {
-    std::cout << "Starting template.\n";
+    std::string dinput{};
+    std::cin >> dinput;
+    std::cout << filterWhitespace(dinput);
     return 0;
 }
