ec9fc6ce2f67745a087790e18636c2352b41b15a
PrzemyslawSochon <przemyslaw.sochon+github@protonmail.ch>
2022-12-11 07:47:46 +0100

    Revert "can't remember"
    This reverts commit 71196d6dad3a19a1e75bba0a50aee17fb99d9814.
    Commited to a wrong repository.

--- main.cpp
+++ main.cpp
@@ -1,24 +1,7 @@
 #include "input.hpp"
-#include <algorithm>
-#include <iostream>
-#include <string>
-
-std::string filterWhitespace(const std::string& str)
-{
-    auto is_ws = [](char c)
-    {
-        return std::isspace(c);
-    };
-    std::string copy{str};
-    copy.erase(std::remove_if(copy.begin(), copy.end(), is_ws), copy.end());
-
-    return copy;
-}
 
 int main()
 {
-    std::string dinput{};
-    std::cin >> dinput;
-    std::cout << filterWhitespace(dinput);
+    std::cout << "Starting template.\n";
     return 0;
 }
