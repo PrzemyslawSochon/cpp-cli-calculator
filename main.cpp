3471f11cf1f60f117f30fbca5b661ad8521ce2c3
PrzemyslawSochon <przemyslaw.sochon+github@protonmail.ch>
2022-12-11 09:29:54 +0100

    brackets verifier v1.0

--- main.cpp
+++ main.cpp
@@ -1,10 +1,43 @@
 #include "input.hpp"
 #include <algorithm> //for std::remove_if
 
+enum class bracketFlags
+{
+    ok,
+    error,
+
+    size
+};
+
 std::string purgeWhiteSpaces(std::string& str)
 {
     str.erase(std::remove_if(str.begin(), str.end(), isspace), str.end());
     return str;
+}
+
+bracketFlags areBracketsCorrect(const std::string& str)
+{
+    for(int i{0};i<str.size();++i)
+    {
+        //if found closing bracket, look for its opening counterpart
+        if(str[i]==')')
+        {
+            static int last_position_mark{i};
+            for (int j{last_position_mark-1};j>=0;--j)
+            {
+                if(str[j]=='(')
+                {
+                    last_position_mark=j;
+                    break;
+                }
+
+                if(j==0)
+                {
+                    return bracketFlags::error;
+                }
+            }
+        }
+    }
 }
 
 
