e3fbedbf3c529259c47327e223218782a9acd3f7
PrzemyslawSochon <przemyslaw.sochon+github@protonmail.ch>
2022-12-11 09:51:31 +0100

    added checking if brackets have pairs todo: upgrade encapsulation checker, app doesn't see anything wrong with ")()(" prompt

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "input.hpp"
 #include <algorithm> //for std::remove_if
 
-enum class bracketFlags
+enum class brackets
 {
     ok,
-    error,
+
+    err_odd_number,
+    err_opening_absence,
 
     size
 };
@@ -15,7 +17,26 @@ std::string purgeWhiteSpaces(std::string& str)
     return str;
 }
 
-bracketFlags areBracketsCorrect(const std::string& str)
+brackets areBracketsEven(const std::string& str)
+{
+    int open_bracket_counter{0};
+    int close_bracket_counter{0};
+
+    for(const char& i : str)
+    {
+        if(i=='(') ++open_bracket_counter;
+        else if (i==')') ++close_bracket_counter;
+    }
+
+    if(open_bracket_counter==close_bracket_counter)
+    {
+        return brackets::ok;
+    }
+    else {return brackets::err_odd_number;}
+}
+
+
+brackets areBracketsEncapsulated(const std::string& str)
 {
     for(int i{0};i<str.size();++i)
     {
@@ -33,11 +54,12 @@ bracketFlags areBracketsCorrect(const std::string& str)
 
                 if(j==0)
                 {
-                    return bracketFlags::error;
+                    return brackets::err_opening_absence;
                 }
             }
         }
     }
+    return brackets::ok;
 }
 
 
@@ -47,7 +69,15 @@ int main()
     std::cout << "Enter the expression: ";
     std::string expression{};
     std::getline(std::cin, expression);
-    std::cout << purgeWhiteSpaces(expression);
+    std::cout << purgeWhiteSpaces(expression) << '\n';
 
+    if(areBracketsEncapsulated(expression)==brackets::ok && areBracketsEven(expression)==brackets::ok)
+    {
+        std::cout << "OK!\n";
+    }
+    else
+    {
+        std::cout << "NOT OK!\n";
+    }
     return 0;
 }
