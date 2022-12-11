562cca8d4e8a8284d2c6f32b4c071b36baf4ee23
PrzemyslawSochon <przemyslaw.sochon+github@protonmail.ch>
2022-12-11 08:45:52 +0100

    added removal of whitespaces from user input

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include "input.hpp"
+#include <algorithm> //for std::remove_if
+
+std::string purgeWhiteSpaces(const std::string& str)
+{
+    std::string str_copy{str};
+    str_copy.erase(std::remove_if(str_copy.begin(), str_copy.end(), isspace), str_copy.end());
+    return str_copy;
+}
+
 
 int main()
 {
-    std::cout << "Starting template.\n";
+
+    std::cout << "Enter the expression: ";
+    std::string expression{};
+    std::getline(std::cin, expression);
+    std::cout << purgeWhiteSpaces(expression);
+
     return 0;
 }
