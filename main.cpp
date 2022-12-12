bffd03432551bb8160151b88b83d836db6b35544
PrzemyslawSochon <przemyslaw.sochon+github@protonmail.ch>
2022-12-12 01:44:40 +0100

    added canceling out leading signs in a row

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include "input.hpp"
 #include <algorithm> //for std::remove_if
 #include <array>
+#include <string>
 #include <vector>
 
 //x_scope can hold vector of coordinates such as [[1,2],[3,4],[10,12]]
-using x_scope = std::vector<std::array<int,2>>;
+using x_scope = std::array<int,2>;
 
-enum class brackets
+enum class Brackets
 {
     ok,
 
@@ -16,12 +17,20 @@ enum class brackets
     size
 };
 
+enum class Sign
+{
+    negative,
+    positive,
+
+    size
+};
+
 inline void purgeWhiteSpaces(std::string& str)
 {
     str.erase(std::remove_if(str.begin(), str.end(), isspace), str.end());
 }
 
-brackets areBracketsEven(const std::string& str)
+Brackets areBracketsEven(const std::string& str)
 {
     int open_bracket_counter{0};
     int close_bracket_counter{0};
@@ -34,17 +43,16 @@ brackets areBracketsEven(const std::string& str)
 
     if(open_bracket_counter==close_bracket_counter)
     {
-        return brackets::ok;
+        return Brackets::ok;
     }
     else
     {
         std::cout << "Brackets aren't paired.\n";
-        return brackets::err_odd_number;
+        return Brackets::err_odd_number;
     }
 }
 
-
-brackets areBracketsEncapsulated(const std::string& str)
+Brackets areBracketsEncapsulated(const std::string& str)
 {
     for(int i{0};i<str.size();++i)
     {
@@ -57,7 +65,7 @@ brackets areBracketsEncapsulated(const std::string& str)
                 {
                     std::cout << "Brackets aren't correctly encapsulated. "
                               << "See closed bracket `)` at position " << i << ".\n";
-                    return brackets::err_opening_absence;
+                    return Brackets::err_opening_absence;
                 }
 
                 if(str[j]=='(')
@@ -67,18 +75,63 @@ brackets areBracketsEncapsulated(const std::string& str)
             }
         }
     }
-    return brackets::ok;
+    return Brackets::ok;
 }
+
+void cancelRowOfSigns(std::string& str)
+{
+    //firstly lets check if there is anything to cancel at all
+    if(str.size()<=1) {return;}
+
+}
+
+void cancelOutSigns(std::string& str)
+{
+    int i{0};
+
+    for(i=0;i<str.size();++i)
+    {
+        int minus_signs{0};
+        if(str[i]=='-' || str[i]=='+')
+        {
+            x_scope signs_position = {0,0};
+
+            int j{};
+            for(j=i; (str[j]=='+' || str[j]=='-' || str[j]=='(') ;++j)
+            {
+                if(str[j]=='-'){++minus_signs;}
+                signs_position = {i,j+1};
+            }
+            if(minus_signs%2==0)
+            {
+                str.erase(signs_position[0], signs_position[1]);
+                str.insert(signs_position[0], "+");
+            }
+            else
+            {
+                str.erase(signs_position[0], signs_position[1]);
+                str.insert(signs_position[0], "-");
+            }
+        }
+    }
+    std::cout << str << '\n';
+}
+
+//order of performing math operations
 
 
 int main()
 {
-
     std::cout << "Enter the expression: ";
     std::string expression{};
     std::getline(std::cin, expression);
+
     purgeWhiteSpaces(expression);
     std::cout << expression << '\n';
+
+    areBracketsEven(expression);
+    areBracketsEncapsulated(expression);
+    cancelOutSigns(expression);
 
     return 0;
 }
