6f429c43e292703f64110aee002b0bb259d90139
PrzemyslawSochon <przemyslaw.sochon+github@protonmail.ch>
2022-12-14 17:16:47 +0100

    beginning of overhauling solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -3,7 +3,117 @@
 #include <cmath>
 #include <iostream>
 
-StringPosition extractFromParenthesis(const std::string& str)
+// order of operations
+// look for closing bracket
+
+bool isOperator(char c)
+{
+    return c == '+' || c == '-' || c == '*' || c == '/' || c == '^' || c == '%' || c == '!';
+}
+
+SignificantExpr parenthesis(std::string &str)
+{
+    std::string value1;
+    char symbol{'+'};
+    std::string value2{0};
+    for (int i{0}; i < str.size(); ++i)
+    {
+        if (str[i] == ')')
+        {
+            for (int j{i - 1}; j >= 0; --j)
+            {
+                if (str[j] == '(')
+                {
+                    for (int k{j + 1}; k < str.size(); ++k)
+                    {
+                        if (isOperator(str[k]))
+                        {
+                            // we've got everything, begin extraction
+                            value1 = str.substr(j + 1), k - j - 1;
+                            symbol = str[k];
+                            value2 = str.substr(k + 1), i - k - 1;
+                        }
+                        else if (str[k] == ')')
+                        {
+                            value1 = str.substr(j + 1), i - j - 1;
+                        }
+                    }
+                    return {value1, symbol, value2};
+                }
+            }
+        }
+    }
+}
+
+SignificantExpr binaryOperators(std::string &str)
+{
+    std::string value1;
+    char symbol{'+'};
+    std::string value2{0};
+    for (int i{0}; i <= str.size(); ++i)
+    {
+        if (isOperator(str[i]))
+        {
+            symbol=str[i];
+            for (int j{i-1}; j >= 0; --j)
+            {
+                if (isOperator(str[j]))
+                {
+                    value1 = str.substr(j + 1, i - j - 1);
+                }
+            }
+            for (int k{i+1}; k <= str.size(); ++k)
+            {
+                if (isOperator(str[k]))
+                {
+                    value2 = str.substr(i + 1, k - i - 1);
+                }
+            }
+            return{value1, symbol, value2};
+        }
+    }
+}
+
+double results(SignificantExpr expr)
+{
+    double value1{std::stod(expr.value1)};
+    char symbol{expr.symbol};
+    double value2{std::stod(expr.value2)};
+
+    switch (symbol)
+    {
+    case '+':
+    {
+        return (value1 + value2);
+    }
+    case '-':
+    {
+        return (value1 - value2);
+    }
+    case '/':
+    {
+        return (value1 / value2);
+    }
+    case '*':
+    {
+        return (value1 * value2);
+    }
+    case '^':
+    {
+        return (pow(value1, value2));
+    }
+    case '%':
+    {
+        return (fmod(value1, value2));
+    }
+    default:
+    {
+        assert(symbol && "Operator not found!");
+    }
+    }
+}
+
+StringPosition extractFromParenthesis(const std::string &str)
 {
     for (int i{0}; i < str.size(); ++i)
     {
@@ -13,7 +123,7 @@ StringPosition extractFromParenthesis(const std::string& str)
             {
                 if (str[j] == '(')
                 {
-                    return {str.substr(j + 1, i - j - 1),j,i};
+                    return {str.substr(j + 1, i - j - 1), j, i};
                 }
             }
             assert(str[i] && "No closing bracket!");
@@ -35,9 +145,9 @@ Expression turnStringIntoExpression(const std::string &str)
         case '^':
         case '%':
         {
-            double variable1{static_cast<double>(std::stoi(str.substr(0, i + 1)))};
-            double variable2{static_cast<double>(std::stoi(str.substr(i + 1)))};
-            char symbol{static_cast<char>(str[i])};
+            double variable1{(std::stod(str.substr(0, i + 1)))};
+            double variable2{(std::stod(str.substr(i + 1)))};
+            char symbol{str[i]};
             return {variable1, variable2, symbol};
         }
         }
@@ -81,8 +191,8 @@ double calculateResultOfExpression(Expression ex)
     return EXIT_FAILURE;
 }
 
-void insertResultToExpression(std::string& expr, double result, int pos1, int pos2)
+void insertResultToExpression(std::string &expr, double result, int pos1, int pos2)
 {
-    expr.erase(pos1, pos2 - pos1 +1);
+    expr.erase(pos1, pos2 - pos1 + 1);
     expr.insert(pos1, std::to_string(result));
 }
--- solution.hpp
+++ solution.hpp
@@ -2,11 +2,18 @@
 #include <string>
 #include <string_view>
 
+struct SignificantExpr
+{
+    std::string value1;
+    char symbol{'+'};
+    std::string value2{0};
+};
+
 struct Expression
 {
     double variable1{1};
     double variable2{1};
-    char symbol{};
+    char symbol{'$'};
 };
 
 struct StringPosition
@@ -22,4 +29,4 @@ Expression turnStringIntoExpression(const std::string &str);
 
 double calculateResultOfExpression(Expression ex);
 
-void insertResultToExpression(std::string& expr, double result, int pos1, int pos2);
+void insertResultToExpression(std::string& expr, double result, int pos1, int pos2);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
