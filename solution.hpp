2be48a29d8882389dc7fd8ea5f586169ca9101fb
PrzemyslawSochon <przemyslaw.sochon+github@protonmail.ch>
2022-12-15 19:09:50 +0100

    deleted old comments

--- solution.hpp
+++ solution.hpp
@@ -9,27 +9,6 @@ struct SignificantExpr
     std::string value2{0};
 };
 
-/* struct Expression
-{
-    double variable1{1};
-    double variable2{1};
-    char symbol{'$'};
-};
-
-struct StringPosition
-{
-    std::string str{};
-    int x1 {};
-    int x2 {};
-}; */
-
-/* StringPosition extractFromParenthesis(const std::string& str);
-
-Expression turnStringIntoExpression(const std::string &str);
-
-double calculateResultOfExpression(Expression ex); */
-
-bool isOperator(char c);
 SignificantExpr parenthesis(const std::string &str);
 SignificantExpr binaryOperators(const std::string &str);
 double results(SignificantExpr expr);
