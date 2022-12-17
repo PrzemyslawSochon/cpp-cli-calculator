965c348ce6839f4b7baf8bef9111629229d4d384
PrzemyslawSochon <przemyslaw.sochon+github@protonmail.ch>
2022-12-17 06:01:35 +0100

    deleted all functions

--- solution.hpp
+++ solution.hpp
@@ -2,13 +2,3 @@
 #include <string>
 #include <string_view>
 
-struct SignificantExpr
-{
-    std::string value1;
-    char symbol{'+'};
-    std::string value2{0};
-};
-
-SignificantExpr solveParenthesis(const std::string &str);
-SignificantExpr binaryOperators(const std::string &str);
-double results(SignificantExpr expr);
